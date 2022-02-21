2c5e55c9b0c9c68ad4ba1fd3511d47390ad8c6f3
Korver <korver2017@gmail.com>
2022-02-22 02:24:32 +0800

    refactor: Replace `var` to `const` or `let`

--- js/main.js
+++ js/main.js
@@ -19,27 +19,30 @@ $(document).ready (function () {
 
     $('#map').css ('box-shadow', '1px 3px 20px -3px rgba(0,0,0,0.75)');
 
-    var myLatLng = {
+    const myLatLng = {
       lat: 23.98439014,
       lng: 121.6115606
     };
 
-    var map = new google.maps.Map (document.getElementById ('map'), {
+    const map = new google.maps.Map (document.getElementById ('map'), {
       center: myLatLng,
       zoom: 14
     });
     
-    var dataLen = data.length;
-    for (var i = 0; i < dataLen; i ++) {
+    const dataLen = data.length;
 
-      var str = {};
-      var place = {};
+    for (let i = 0; i < dataLen; i ++) {
+
+      let str = {};
+      let place = {};
 
       place.lat = parseFloat (data[i]['Latitude']);
       place.lng = parseFloat (data[i]['Longitude']);
+
       str.map = map;
       str.title = data[i]['Name'];
       str.position = place;
+
       new google.maps.Marker (str);
     }
   });
