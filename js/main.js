4b20006e20aabee41d263199f174cc0046eca790
Korver <korver2017@gmail.com>
2022-02-22 03:27:05 +0800

    refactor: Add comments

--- js/main.js
+++ js/main.js
@@ -14,15 +14,15 @@ import locations from './data.js';
  *
  */
 $(document).ready (function () {
-
   $('.search').on ('click', function initMap () {
-
     $('#map').css ('box-shadow', '1px 3px 20px -3px rgba(0,0,0,0.75)');
 
+    // Initialize map.
     const center = {lat: 23.98439014, lng: 121.6115606}
         , map = new google.maps.Map ($('#map')[0], {center, zoom: 14})
         ;
 
+    // Mark locations on map.
     locations.forEach (location => {
 
       let position = {}
